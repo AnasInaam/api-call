aa73826b135ed39928427a8edcc76f4954b72a1b
Mohammad Anas <mirzaanas937@gmail.com>
2025-06-16 20:21:13 +0530

    Refactor code structure and remove redundant code blocks

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,11 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#2563eb" />
+        {/* You can add more icons here for better PWA support */}
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
